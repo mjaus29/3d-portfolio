513cfca950295c087c27dca6f2d120e51adc1b8f
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-04-22 20:28:50 +0800

    Add unique keys to map iterations to fix React warnings
    Added missing `key` attributes in various components (`AnimatedCounter`, `LogoSection`, and `Testimonials`) to address React's warning about unique key props in list renderings. This ensures better handling of rendered elements and improves performance.

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -7,7 +7,10 @@ const AnimatedCounter = () => {
     <div id="counter" className="padding-x-lg xl:mt-0 mt-32">
       <div className="mx-auto grid-4-cols">
         {counterItems.map((item) => (
-          <div className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center">
+          <div
+            className="bg-zinc-900 rounded-lg p-10 flex flex-col justify-center"
+            key={item.label}
+          >
             <div
               key={counterItems.label}
               className="counter-number text-white text-5xl font-bold mb-2"
--- src/sections/Testimonials.jsx
+++ src/sections/Testimonials.jsx
@@ -14,7 +14,7 @@ const Testimonials = () => {
 
         <div className="lg:columns-3 md:columns-2 columns-1 mt-16">
           {testimonials.map(({ imgPath, name, mentions, review }) => (
-            <GlowCard card={{ review }}>
+            <GlowCard key={name} card={{ review }}>
               <div className="flex items-center gap-3">
                 <div>
                   <img src={imgPath} alt={name} />
